d59e71c8ec71b6aa65eb04c419b0f7b07e33ce72
theogirl <theogirl@outlook.com>
2015-06-18 18:35:04 -0400

    final working

--- js/tube.js
+++ js/tube.js
@@ -1,20 +1,24 @@
 $(function() {
  
+ $('.search-results').hide();
+
+ //----Global Variables-------//
  var myData = '';
  var searchTerm = '';
  var videoID = '';
  var videoTitle = '';
  var videoThumb = '';
+ var videoAuthor = ''
  var videoURL = '';
 
 
 //----EVENTS-------//
 $('#submit-btn').click(function() {
-	$('.search-results').empty();
-	searchTerm = $('#query').val();
+	$('.search-results').empty(); //this clears the results div each time so that the videos don't all add together
+	searchTerm = $('#query').val();//grabs user's input
 	getResults();
-});
 
+});
 
 
 //----FUNCTION definitions---//
@@ -22,28 +26,36 @@ $('#submit-btn').click(function() {
 
 function getResults(data) {
 
-var params = { part: 'snippet', type: 'video', q: searchTerm, order: 'rating', relevanceLanguage: 'en', maxResults: '1', key: 'AIzaSyABTE4fBQBAXSrX18w3QPt4zhNBZPrWuyY' }; // a JS object containing name:value pairs
+var params = { part: 'snippet', type: 'video', q: searchTerm, order: 'viewCount', relevanceLanguage: 'en', maxResults: '10', fields: 'items(id(videoId), snippet(title, channelTitle, thumbnails))', key: 'AIzaSyABTE4fBQBAXSrX18w3QPt4zhNBZPrWuyY' }; // a JS object containing name:value pairs
 var url = 'https://www.googleapis.com/youtube/v3/search?';
-	$.getJSON(url, params, function(data) {
+
+$.getJSON(url, params, function(data) {
 			myData = data.items; //the array returned by the search
 			//console.log(myData[0]);
-			videoID = myData[0].id.videoId;
-			videoTitle = myData[0].snippet.title;
-			videoThumb = myData[0].snippet.thumbnails.default.url;
-			showResults();
+			loopAndParse();
+
 	});
 }
 
+function loopAndParse(data) {
+	for (var i=0; i < 10; i++) {
+		videoID = myData[i].id.videoId;
+		videoTitle = myData[i].snippet.title;
+		videoAuthor = myData[i].snippet.channelTitle;
+		videoThumb = myData[i].snippet.thumbnails.default.url;
+		showResults();
+	}
+}
+
 function showResults(data) {
+	
+	$('.search-results').show();
 	var html = '<ul>';
 	
-    html += '<a href="https://www.youtube.com/watch?v='+videoID+'"'+' class="video-link"><li class="video-list-item"><div class="video-img"><img src="'+videoThumb+'"></div><div class="video-description">'+videoTitle+'</div></li></a>';
+    html += '<a href="https://www.youtube.com/watch?v='+videoID+'"'+' class="video-link"><li class="video-list-item"><div class="video-img"><img src="'+videoThumb+'"></div><div class="video-description"><span style="font-weight:bold;">'+videoTitle+'</span><br />Posted by: '+videoAuthor+'</div></li></a>';
 	html += '</ul>';
 
 	$('.search-results').append(html);
-
-
-	//$('.search-list').append('<a href="https://www.youtube.com/watch?v='+videoID+'"'+' class="video-link"><li class="video-list-item"><div class="video-img"><img src="'+videoThumb+'"></div><div class="video-description">'+videoTitle+'</div></li></a>');
 
 }
 
