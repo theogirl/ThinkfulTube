91df824b5922314d78c21250a9b760384af77861
theogirl <theogirl@outlook.com>
2015-06-18 01:03:29 -0400

    one object working

--- js/tube.js
+++ js/tube.js
@@ -2,6 +2,10 @@ $(function() {
  
  var myData = '';
  var searchTerm = '';
+ var videoID = '';
+ var videoTitle = '';
+ var videoThumb = '';
+ var videoURL = '';
 
 
 //----EVENTS-------//
@@ -14,21 +18,35 @@ $('#submit-btn').click(function() {
 
 
 //----FUNCTION definitions---//
-function showResults(data) {
-	$.each(myData, function(index, value) { 
-		$('.search-results').append(value.Title + '<br />');
-	})
-}
+
 
 function getResults(data) {
 
-var params = { s: searchTerm, r: 'json' }; // a JS object containing name:value pairs
-var url = 'http://www.omdbapi.com';
+var params = { part: 'snippet', type: 'video', q: searchTerm, order: 'rating', relevanceLanguage: 'en', maxResults: '1', key: 'AIzaSyABTE4fBQBAXSrX18w3QPt4zhNBZPrWuyY' }; // a JS object containing name:value pairs
+var url = 'https://www.googleapis.com/youtube/v3/search?';
 	$.getJSON(url, params, function(data) {
-			myData = data.Search;
+			myData = data.items; //the array returned by the search
+			//console.log(myData[0]);
+			videoID = myData[0].id.videoId;
+			videoTitle = myData[0].snippet.title;
+			videoThumb = myData[0].snippet.thumbnails.default.url;
 			showResults();
 	});
 }
 
+function showResults(data) {
+	var html = '<ul>';
+	
+    html += '<a href="https://www.youtube.com/watch?v='+videoID+'"'+' class="video-link"><li class="video-list-item"><div class="video-img"><img src="'+videoThumb+'"></div><div class="video-description">'+videoTitle+'</div></li></a>';
+	html += '</ul>';
+
+	$('.search-results').append(html);
+
+
+	//$('.search-list').append('<a href="https://www.youtube.com/watch?v='+videoID+'"'+' class="video-link"><li class="video-list-item"><div class="video-img"><img src="'+videoThumb+'"></div><div class="video-description">'+videoTitle+'</div></li></a>');
+
+}
+
 
 }) //end document ready
+
